092bdf141245e3e95ff0304addab34304893ba22
Drew Lofland <alofland25@gmail.com>
2023-01-25 14:42:57 -0500

    finished exercise

---
+++ Fizzbuzz2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Fizzbuzz2
+{
+    internal class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            //enter an integer
+            Console.WriteLine($"enter number");
+            var input = int.Parse(Console.ReadLine());
+
+            //test if integers are divisible by 3 and or 5
+            Console.WriteLine(Whiteboard(input));
+        }
+
+
+        public static string Whiteboard(int x)
+        {
+            //test if integers are divisible by 3 and or 5 method
+            if ((x % 3 == 0) && (x % 5 == 0))
+            {
+                return "fizzbuzz";
+            }
+
+            else if (x % 3 == 0)
+            {
+                return "fizz";
+            }
+            else if (x % 5 == 0)
+            {
+                return "buzz";
+            }
+            else
+            {
+                return "nothing";
+            }
+
+        }
+    }
+
+
+
+}
+
